419edf5b3b6127214002b37d3f65cd2075cca9a7
TRAORE <e.traore@servicecivique.gouv.ci>
2026-03-31 19:02:37 +0000

    version 1

--- modules/loader.py
+++ modules/loader.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 def load_data():
-    df = pd.read_csv("data/Base_paneliste.csv")
+    try:
+        df = pd.read_csv("data/Base_paneliste.csv", encoding="utf-8")
+    except:
+        df = pd.read_csv("data/Base_paneliste.csv", encoding="latin-1")
+
     df.columns = df.columns.str.lower().str.strip()
     return df
